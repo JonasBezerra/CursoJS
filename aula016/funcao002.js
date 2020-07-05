17f532e493fd9b3dfb74cebbe057e800247f5451
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-04 21:52:55 -0400

    apagando pastas

--- aula016/funcao002.js
+++
@@ -1,5 +0,0 @@
-function soma(n1 = 0,n2 = 0){ // caso ele nao passe um dos parametros. n1 ou n2 vale 0
-    return n1 + n2;
-}
-
-console.log(`A soma é ${soma(30,1)}`)
--- aula016/funcao003.js
+++
@@ -1,5 +0,0 @@
-let v = function(x) { // Jogando uma function dentro de uma variavel
-    return x*2; 
-}
-
-console.log(`O dobro é ${v(5)}`);
